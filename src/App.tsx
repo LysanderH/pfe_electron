e25c7f17c4488ec152b7b0077170f2dc30026502
Lysander Hans <34277017+LysanderH@users.noreply.github.com>
2021-05-21 11:32:46 +0200

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import './styles/app.global.scss';
 import LessonList from './pages/LessonList';
+import PrivateRoute from './utils/PrivateRoute';
+import Exercise from './pages/Exercise';
+import ExerciseList from './pages/ExerciseList';
+import CreateExercise from './pages/CreateExercise';
+import ClassList from './pages/ClassList';
+import Preferences from './pages/Preferences';
+import CreateClass from './pages/CreateClass';
+import MyClass from './pages/MyClass';
+import Logout from './utils/Logout';
+import StartConference from './pages/StartConference';
+import Conference from './pages/Conference';
 
 /**
  * Main component
@@ -32,8 +43,20 @@ export default function App() {
           <Login login={login} />
         </Route>
         <Route path="/register" component={Register} />
-        <Route path="/lesson-list" component={LessonList} />
-        <Route path="/" component={Menu} />
+        <Route path="/logout">
+          <Logout setLoggedIn={setLoggedIn} />
+        </Route>
+        <PrivateRoute path="/preferences" component={Preferences} />
+        <PrivateRoute path="/lessons" component={LessonList} />
+        <PrivateRoute path="/classes/create" component={CreateClass} />
+        <PrivateRoute path="/classes/show" component={MyClass} />
+        <PrivateRoute path="/classes" component={ClassList} />
+        <PrivateRoute path="/exercises/create" component={CreateExercise} />
+        <PrivateRoute path="/exercises/show" component={Exercise} />
+        <PrivateRoute path="/exercises" component={ExerciseList} />
+        <PrivateRoute path="/start-conference" component={StartConference} />
+        <PrivateRoute path="/conference" component={Conference} />
+        <PrivateRoute path="/" component={Menu} />
       </Switch>
     </Router>
   );
