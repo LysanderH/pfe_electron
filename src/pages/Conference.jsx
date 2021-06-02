d5240f0905b953fa8ffc10a7833524f94e5aa859
Lysander Hans <34277017+LysanderH@users.noreply.github.com>
2021-06-02 09:25:51 +0200

    Added conferencing

--- src/pages/Conference.jsx
+++ src/pages/Conference.jsx
@@ -1,127 +1,120 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Echo from 'laravel-echo';
 import axios from 'axios';
+import Chessboard from 'chessboardjsx';
+import { ChessInstance, ShortMove } from 'chess.js';
 import apiClient from '../utils/apiClient';
 import styles from '../styles/pages/Conference.scss';
 
-// window.Pusher = require('pusher-js');
-
-// const token = sessionStorage.getItem('user') ?? '';
-
-// axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-// window.Echo = new Echo({
-//   broadcaster: 'pusher',
-//   // host: 'https://api.localhost',
-//   key: 'bc9ce7f1e2a8c4c77e2f',
-//   cluster: 'eu',
-//   // auth: {
-//   //   Authorization: `Bearer ${token}`,
-//   // },
-// });
-
-// console.log(window.Echo);
+const Chess = require('chess.js');
 
 export default function Conference() {
-  const [hasMedia, setHasMedia] = useState(false);
-  const [video, setVideo] = useState();
-  const [redirect, setRedirect] = useState(false);
-  const [localStream, setLocalStream] = useState();
   const [roomId, setRoomId] = useState();
+  const [jitsi, setJitsi] = useState({});
+  const [user, setUser] = useState();
 
-  useEffect(() => {
-    const peerConnection = new RTCPeerConnection({
-      iceServers: [
-        {
-          urls: [
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-          ],
-        },
-      ],
-      iceCandidatePoolSize: 5,
+  const jitsiContainerId = 'jitsi-container-id';
+
+  const loadJitsiScript = () => {
+    let resolveLoadJitsiScriptPromise = null;
+
+    const loadJitsiScriptPromise = new Promise((resolve) => {
+      resolveLoadJitsiScriptPromise = resolve;
     });
 
-    const request = (offerer) => {
-      apiClient
-        .post('/rooms', {
-          offerer: JSON.stringify(offerer),
-          group: sessionStorage.getItem('user'),
-        })
-        .then((response) => {
-          setRoomId(response.data.room.id);
-          // console.log(window.Echo.private(`room.${response.data.room.id}`));
-          window.Echo.channel(`room.${response.data.room.id}`).listen(
-            'UserConnectedToRoom',
-            (obj) => {
-              console.log(obj);
-            }
-          );
-          return null;
-        })
-        .catch((error) => console.error(error));
-    };
+    const script = document.createElement('script');
+    script.src = 'https://meet.jit.si/external_api.js';
+    script.async = true;
+    script.onload = resolveLoadJitsiScriptPromise;
+    document.body.appendChild(script);
 
-    peerConnection
-      .createOffer()
-      .then((response) => {
-        const offererDescription = response;
-        const offerer = {
-          sdp: offererDescription.sdp,
-          type: offererDescription.type,
-        };
-        peerConnection.setLocalDescription(offererDescription);
-        console.log(peerConnection);
-        console.log(offerer);
-
-        request(offerer);
-        return null;
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
-  const myVideo = useCallback((node) => {
-    if (node !== null) {
-      if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices
-          .getUserMedia({ video: true, audio: true })
-          .then((stream) => {
-            node.srcObject = stream;
-            setVideo(node);
-
-            node.play();
-
-            return null;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    }
-  });
+    return loadJitsiScriptPromise;
+  };
 
   const disconnect = (e) => {
     e.preventDefault();
-    console.log(video);
-    video.pause();
-    video.srcObject = null;
-    // .stop();
+    return <Redirect to="/" />;
+  };
+
+  const initialiseJitsi = async (room, currentUser) => {
+    if (!window.JitsiMeetExternalAPI) {
+      await loadJitsiScript();
+    }
+
+    const jitsi = new window.JitsiMeetExternalAPI('meet.jit.si', {
+      parentNode: document.getElementById(jitsiContainerId),
+      roomName: `ChessTeachingTool${room}`,
+      userInfo: {
+        displayName: currentUser.name,
+        email: currentUser.email,
+      },
+      width: 700,
+      height: 700,
+    });
+
+    setJitsi(jitsi);
+  };
+
+  useEffect(() => {
+    apiClient
+      .post('/rooms', {
+        offerer: JSON.stringify({}),
+        group: sessionStorage.getItem('user'),
+      })
+      .then((response) => {
+        setRoomId(response.data.room.id);
+        setUser(response.data.user.name);
+        initialiseJitsi(response.data.room.id, response.data.user);
+        return null;
+      })
+      .catch((error) => console.error(error));
+
+    return () => jitsi?.dispose?.();
+  }, []);
+
+  const handleAPI = (JitsiMeetAPI) => {
+    JitsiMeetAPI.executeCommand('toggleVideo');
   };
 
   const toggleVideo = (e) => {};
   const toggleMic = (e) => {};
+
+  const [chess] = useState(
+    new Chess('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+  );
+  const [fen, setFen] = useState(chess.fen());
+
+  const handleMove = (newMove) => {
+    // const moves = chess.moves();
+    chess.move(newMove);
+    console.log(chess.fen());
+    setFen(chess.fen());
+  };
 
   return (
     <section className={styles.chat}>
       <h2 className={styles.chat__title}>
         Identifiant de conférence&nbsp;: {roomId ?? ''}
       </h2>
-      <video className={styles.chat__video} ref={myVideo}>
-        <track kind="captions" srcLang="en" />
-        Sorry, your browser doesn’t support embedded videos.
-      </video>
-      <div className={styles.chat__chessboard}>Échiquier</div>
+      <div
+        id={jitsiContainerId}
+        className={styles.chat__video}
+        style={{ height: 720, width: '100%' }}
+      />
+      <Chessboard
+        position={fen}
+        sparePieces
+        className={styles.chat__chessboard}
+        onDrop={(move) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            // This promotion attribute changes pawns to a queen if they reach the other side of the board.
+            promotion: 'q',
+          })
+        }
+      />
       <aside className={styles.chat__controls}>
         <button
           type="button"
